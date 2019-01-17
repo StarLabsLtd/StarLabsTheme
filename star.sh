0e7f46c920bdebd6adc4ab63c507ec638ed609a6
Sean Rhodes <sean@starlabs.systems>
2019-01-17 12:01:28 +0000

    Backing up

--- star.sh
+++ star.sh
@@ -7,11 +7,28 @@ mv src/gnome-shell/*.svg src/gnome-shell/assets
 cp -r upstream/yaru/gtk/src/light/* src/
 
 # Change Ubuntu Colors
-sed -i 's/ubuntu-colors/starlabs-colors/g' src/gnome-shell/gnome-shell-sass/_colors.scss src/gtk-3.0/_colors.scss src/gtk-3.20/_colors.scss
-sed -i 's/orange/blue0/g' src/gnome-shell/gnome-shell-sass/_colors.scss src/gtk-3.0/_colors.scss src/gtk-3.20/_colors.scss
+# sed -i "s/'colors';/'starlabs-colors';/g" src/gtk-3.20/gtk.scss
+# sed -i "s/'colors';/'starlabs-colors';/g" src/gtk-3.20/gtk-dark.scss
+# sed -i "s/'colors';/'starlabs-colors';/g" src/gtk-3.0/gtk.scss
+# sed -i "s/'colors';/'starlabs-colors';/g" src/gtk-3.0/gtk-dark.scss
+# cp src/gnome-shell/gnome-shell.scss src/gnome-shell/gnome-shell-dark.scss
+# sed -i 's/dark/light/g' src/gnome-shell/gnome-shell.scss
+# sed -i 's/_ubuntu-colors/_starlabs-colors/g' src/gnome-shell/gnome-shell.scss
+# sed -i 's/_ubuntu-colors/_starlabs-colors/g' src/gnome-shell/gnome-shell-dark.scss
+
+
+
+# sed -i "s/'colors';/'starlabs-colors';/g" src/gnome-shell/gnome-shell.scss
+# sed -i "s/'colors';/'starlabs-colors';/g" src/gnome-shell/gnome-shell-dark.scss
+
+
+# sed -i 's/ubuntu-colors/starlabs-colors/g' src/gnome-shell/gnome-shell-sass/_colors.scss src/gtk-3.0/_colors.scss src/gtk-3.20/_colors.scss
+# sed -i 's/orange/blue0/g' src/gnome-shell/gnome-shell-sass/_colors.scss src/gtk-3.0/_colors.scss src/gtk-3.20/_colors.scss
 
 sed -i 's/font-family: Ubuntu/font-family: Star Labs/g' src/gnome-shell/gnome-shell-sass/_common.scss
-sed -i 's/#2C001E/#0d1f2d/g' src/gnome-shell/gnome-shell-sass/_common.scss
+# sed -i 's/#2C001E/$terminal_bg_color/g' src/gnome-shell/gnome-shell-sass/_common.scss
+
+
 # EXPERIMENTAL
 # sed -i 's/#2e3436/#0d1f3d/g' src/gnome-shell/gnome-shell-sass/_common.scss
 # sed -i 's/#666666/#0d1f2d/g' src/gnome-shell/gnome-shell-sass/_common.scss
