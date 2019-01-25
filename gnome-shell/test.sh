3b81390688dfd2517caefddf6b6a1a0b18ff4c2d
Sean Rhodes <sean@starlabs.systems>
2019-01-25 16:18:47 +0000

    Install Test

--- gnome-shell/test.sh
+++ gnome-shell/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sassc gnome-shell.scss gnome-shell.css
 glib-compile-resources gnome-shell-theme.gresource.xml
-sudo cp gnome-shell.css /usr/share/themes/StarLabs/gnome-shell/gnome-shell.css
-sudo cp gnome-shell-theme.gresource /usr/share/themes/StarLabs/gnome-shell/gnome-shell-theme.gresource
+sudo cp gnome-shell.css /usr/share/themes/Laboratory/gnome-shell/gnome-shell.css
+sudo cp gnome-shell-theme.gresource /usr/share/themes/Laboratory/gnome-shell/gnome-shell-theme.gresource
