1630f80019e2e5a5b46de05236915bce72882bf1
Sean Rhodes <sean@starlabs.systems>
2019-07-24 19:30:31 +0100

    Tweaked

--- assemble.sh
+++ assemble.sh
@@ -52,17 +52,6 @@ function renderIcon() {
 	echo "Name=$1" >> "icons/$1/index.theme"
 	echo "Inherits=Humanity,hicolor" >> "icons/$1/index.theme"
 	echo "Example=folder" >> "icons/$1/index.theme"
-
-	for list in icons/src/lists/*/*; do
-		while read ALIAS ; do
-			FROM=${ALIAS% *}
-			TO=${ALIAS#* }
-			if [ -e "icons/$theme/cursors/$FROM" ] ; then
-				continue
-			fi
-			ln -s "$TO" "icons/$theme/cursors/$FROM"
-		done < "$list"
-	done
 }
 function shapetastic() {
 	for svg in icons/src/fullcolor/*/*.svg; do
@@ -87,26 +76,28 @@ function shapetastic() {
 }
 
 function symlink() {
-        while read ALIAS ; do
-                variant=$(echo "$list" | cut -d '/' -f4)
-                type=$(echo "$list" | cut -d '/' -f5| cut -d '.' -f1)
-                FROM=${ALIAS% *}
-                TO=${ALIAS#* }
-                if [ -e "icons/$1/$path/$sub/$FROM" ]; then
-                        continue
-                fi
-                if [[ "$variant" == 'fullcolor' ]]; then
-                        for size in 8 16 24 32 48 256; do
-                                for scale in @1x @2x; do
-                                        scale=${scale#"@1x"}
-                                        path=icons/"$1"/"$size"x"$size""$scale"/"$type"
-                                        ln -s "$TO" "$path/$FROM"
-                                done
-                        done
-                elif [[ "$path" == 'scalable' ]]; then
-                        ln -s "$TO" "icons/$1/scalable/$FROM"
-                fi
-        done < "$list"
+	for list in icons/src/lists/*/*; do
+		while read ALIAS ; do
+			variant=$(echo "$list" | cut -d '/' -f4)
+			type=$(echo "$list" | cut -d '/' -f5| cut -d '.' -f1)
+			FROM=${ALIAS% *}
+			TO=${ALIAS#* }
+			if [ -e "icons/$1/$path/$sub/$FROM" ]; then
+				continue
+			fi
+			if [[ "$variant" == 'fullcolor' ]]; then
+				for size in 8 16 24 32 48 256; do
+					for scale in @1x @2x; do
+						scale=${scale#"@1x"}
+						path=icons/"$1"/"$size"x"$size""$scale"/"$type"
+						ln -s "$TO" "$path/$FROM"
+					done
+				done
+			elif [[ "$path" == 'scalable' ]]; then
+				ln -s "$TO" "icons/$1/scalable/$FROM"
+			fi
+		done < "$list"
+	done
 }
 
 function exportwallpaper() {
