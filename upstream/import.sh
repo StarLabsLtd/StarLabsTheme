511c0d95440a0844adda5c7cdd70cd7c2f3b8d3b
Sean Rhodes <sean@starlabs.systems>
2020-04-16 07:47:41 +0100

    Various updates

--- upstream/import.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp yaru/gtk/src/default/gtk-3.20/_apps.scss ../gtk/Master/gtk-3.0/_apps.scss
-sed -i 's/ubuntu-colors/starlabs/g' ../gtk/Master/gtk-3.0/_apps.scss
-cp yaru/gtk/src/default/gtk-3.20/_colors.scss ../gtk/Master/gtk-3.0/_colors.scss
-sed -i 's/ubuntu-colors/starlabs/g' ../gtk/Master/gtk-3.0/_colors.scss
-
-cp yaru/gnome-shell/src/gnome-shell-sass/_colors.scss ../gnome-shell/Master/gnome-shell-sass/_colors.scss
-sed -i 's/ubuntu-colors/starlabs/g' ../gnome-shell/Master/gnome-shell-sass/_colors.scss
-
